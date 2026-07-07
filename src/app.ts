3b764248fe9e1dbaa9dc793a4366bc457542e0a0
Mahmud Hossain Sushmoy <sushmoy@duck.com>
2026-07-07 09:54:36 +0600

    separate the app listen

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+// src/index.ts
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import contactRoutes from "./routes/contactRoutes";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req: Request, res: Response) => {
+  res.json({ status: "API is running" });
+});
+
+app.use("/api/contacts", contactRoutes);
+
+export default app;
